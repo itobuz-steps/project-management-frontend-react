fb419b8c9cc4da2319ad9a58743bf8065725868e
Devjyoti Banerjee <devjyoti.banerjee@itobuz.com>
2026-01-22 15:10:14 +0530

    PM-276 fix: main layout and app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.scss";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
 
 function App() {
-  return <h1 className="text-red-500">Hello world</h1>;
+  const isAuthenticated = true;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/login" element={<Login />} /> */}
+
+        {isAuthenticated && (
+          <Route
+            path="/*"
+            element={
+              <MainLayout>
+                <h1 className="text-2xl font-semibold">Hello world</h1>
+              </MainLayout>
+            }
+          />
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
