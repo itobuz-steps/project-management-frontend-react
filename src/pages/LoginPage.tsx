2bd313603d9e0eb3036f8a2c93c76a372057245c
Sujal Gupta <sujal.gupta@itobuz.com>
2026-02-16 11:44:12 +0530

    PM-371 fix: add redirect to login and signup page if already logged in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,19 @@ import { AuthLayout } from '../components/auth/AuthLayout';
 import { TabSwitcher } from '../components/auth/TabSwitcher';
 import bgImage from '../assets/bg-signup.svg';
 import { LoginContainer } from '../components/auth/LoginContainer';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export function LoginPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      navigate('/for-you');
+    }
+  }, [navigate]);
+
   return (
     <AuthLayout backgroundImageUrl={bgImage}>
       <div className="relative flex h-full w-full flex-col items-center justify-center gap-10">
