6b6ab7dbcf2be002deeeb1627420d5c048bf7d77
Sanket Ganguly <sanket@itobuz.com>
2026-02-17 16:22:37 +0530

    PM-368 fix: refactor create sprint form to use antd form

--- src/components/backlog/CreateSprintForm.tsx
+++ src/components/backlog/CreateSprintForm.tsx
@@ -1,4 +1,4 @@
-import { InputNumber } from 'antd';
+import { Button, Form, InputNumber } from 'antd';
 import type { Sprint } from '../../services/types/sprints.types';
 import { SprintButton } from './SprintButton';
 import { useState } from 'react';
@@ -15,7 +15,7 @@ export function CreateSprintForm({
   createSprintHandler,
 }: CreateSprintFormProps) {
   const [showForm, setShowForm] = useState(false);
-  const [storyPoint, setStoryPoint] = useState<number | null>(null);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <>
@@ -26,37 +26,66 @@ export function CreateSprintForm({
       )}
 
       {showForm && (
-        <>
+        <Form
+          onFinish={(values) => {
+            createSprintHandler(values.storyPoint);
+            setShowForm(false);
+          }}
+          onFinishFailed={() => {
+            toast.error('Please enter a valid integer for story points');
+          }}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '0.5rem',
+          }}
+        >
           <div className="flex items-center gap-4">
             <X
               className="-mr-2 size-4 cursor-pointer"
               onClick={() => setShowForm(false)}
             />
-            <InputNumber
-              min={0}
-              onChange={(value) => setStoryPoint(value)}
-              placeholder="Story Points"
-              style={{ width: '120px' }}
-            />{' '}
+            <Form.Item
+              name={'storyPoint'}
+              rules={[
+                {
+                  type: 'number',
+                  min: 0,
+                  message: '',
+                },
+                { required: true, message: '' },
+              ]}
+              style={{ margin: 0 }}
+            >
+              <InputNumber
+                min={0}
+                type="number"
+                placeholder="Story Points"
+                style={{ width: '120px' }}
+              />
+            </Form.Item>{' '}
           </div>
-          <SprintButton
-            onClick={() => {
-              if (
-                !storyPoint ||
-                !Number.isInteger(storyPoint) ||
-                storyPoint < 0
-              ) {
-                toast.error('Please enter a valid number for story points');
-                return;
-              }
 
-              createSprintHandler(storyPoint);
-              setShowForm(false);
-            }}
-          >
-            Create
-          </SprintButton>
-        </>
+          <Form.Item style={{ margin: 0 }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{
+                borderRadius: 'var(--radius-sm)',
+                backgroundColor: hovered
+                  ? 'var(--color-primary-500)'
+                  : 'var(--color-primary-400)',
+                paddingBlock: '0.125rem',
+                paddingInline: '0.5rem',
+              }}
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+            >
+              Create
+            </Button>
+          </Form.Item>
+        </Form>
       )}
     </>
   );
