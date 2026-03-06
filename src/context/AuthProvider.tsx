996b089afadc17b103fce14c700ffa5f009fbd72
Sujal Gupta <sujal.gupta@itobuz.com>
2026-03-06 16:44:13 +0530

    fix: PM-32 superadmin permissions are only visible after selecting project

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { Role } from '../services/types/user';
 import { AuthContext } from './AuthContext';
+import userService from '../services/userService';
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [role, setRole] = useState<Role>('member');
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      const userInfo = await userService.getUserInfo();
+      if (userInfo.result.role === 'superadmin') {
+        setRole('superadmin');
+      }
+    };
+    fetchUserInfo();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ role, setRole }}>
--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,12 +1,21 @@
 import type { Role } from '../services/types/user';
 
 // permissions.ts
-export type Permission = 'ADD_COLUMN' | 'SEND_INVITE';
+export type Permission =
+  | 'ADD_COLUMN'
+  | 'SEND_INVITE'
+  | 'CREATE_PROJECT'
+  | 'CREATE_WORKSPACE';
 
 const rolePermissions: Record<Role, Permission[]> = {
   member: [],
   admin: ['ADD_COLUMN', 'SEND_INVITE'],
-  superadmin: ['ADD_COLUMN', 'SEND_INVITE'],
+  superadmin: [
+    'ADD_COLUMN',
+    'SEND_INVITE',
+    'CREATE_PROJECT',
+    'CREATE_WORKSPACE',
+  ],
 };
 
 export const getPermissionsByRole = (role: Role) => rolePermissions[role] ?? [];
