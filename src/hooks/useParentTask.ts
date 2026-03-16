2de8928bb3db491824add891d622c25484733710
Devjyoti Banerjee <devjyoti.banerjee@itobuz.com>
2026-03-16 13:40:16 +0530

    feat: PM-57 migrate fetch task and update task hooks to react query

--- src/hooks/useParentTask.ts
+++ src/hooks/useParentTask.ts
@@ -1,29 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import type { TaskPopulated } from '../services/types/tasks.types';
 import getTaskById from '../services/taskService';
 import { message } from 'antd';
+import { AxiosError } from 'axios';
 
 export function useParentTask(parentTaskId?: string | null) {
-  const [parentTask, setParentTask] = useState<TaskPopulated | null>(null);
+  const parentTaskQuery = useQuery({
+    queryKey: ['task', parentTaskId],
+    queryFn: () => getTaskById(parentTaskId as string),
+    enabled: !!parentTaskId,
+  });
 
   useEffect(() => {
-    async function load() {
-      if (!parentTaskId) {
-        setParentTask(null);
-        return;
-      }
+    if (parentTaskQuery.error) {
+      const error = parentTaskQuery.error as AxiosError<{ message?: string }>;
 
-      try {
-        const task = await getTaskById(parentTaskId as string);
-        setParentTask(task);
-      } catch {
-        message.error('Failed to get parent task');
-        setParentTask(null);
-      }
+      message.error(
+        error.response?.data?.message ||
+          error.message ||
+          'Failed to get parent task'
+      );
     }
+  }, [parentTaskQuery.error]);
 
-    load();
-  }, [parentTaskId]);
+  const parentTask: TaskPopulated | null = parentTaskQuery.data ?? null;
 
   return parentTask;
 }
