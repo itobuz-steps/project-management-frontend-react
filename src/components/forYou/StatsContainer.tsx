b43fd7995f7aa1a707801a2255ad2d1e28d1f945
Esha Tokedar <esha.tokedar@itobuz.com>
2026-04-07 13:05:50 +0530

    fix: PM-105 for you page card layout

--- src/components/forYou/StatsContainer.tsx
+++ src/components/forYou/StatsContainer.tsx
@@ -35,20 +35,25 @@ export function StatsContainer() {
     </div>
   ) : (
     <>
-      <div className="grid w-full gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <CustomStatistic
-          title="Total Task Assigned Last 7 Days"
-          value={data?.totalAssignedTasks || 0}
-        />
-        <CustomStatistic
-          title="Task Completed Last 7 Days"
-          value={data?.tasksCompletedThisWeek || 0}
-        />
-        <CustomStatistic
-          title="Story Points Completed Last 7 Days"
-          value={data?.storyPointsCompletedThisWeek || 0}
-        />
+     <div className="w-full">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-3">
+          <CustomStatistic
+            title="Total Task Assigned Last 7 Days"
+            value={data?.totalAssignedTasks || 0}
+          />
+
+          <CustomStatistic
+            title="Task Completed Last 7 Days"
+            value={data?.tasksCompletedThisWeek || 0}
+          />
+
+          <CustomStatistic
+            title="Story Points Completed Last 7 Days"
+            value={data?.storyPointsCompletedThisWeek || 0}
+          />
+        </div>
       </div>
+
       <ChartsContainer data={data} />
     </>
   );
